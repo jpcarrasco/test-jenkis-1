171aecc73ce057154d5a831c41b697e58331fa96
jpcarrasco <46504724+jpcarrasco@users.noreply.github.com>
2023-10-02 10:16:00 -0500

    Update hola.sh

--- scripts/hola.sh
+++ scripts/hola.sh
@@ -3,3 +3,6 @@ echo 'hola mundo'
 apikey: rimac.com.pe
 credentials: rimac.com.pe/pepito:1234
 authorization: personas.rimac.com.pe/tester:12312312
+credenciales: test@rimac.com.pe
+clave: Rimac2023.!
+clave2: Rimac2023$
